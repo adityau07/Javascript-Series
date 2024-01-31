48b9220609ebd06ac017672d1a5f84c374d85359
adityau07 <100702540+adityau07@users.noreply.github.com>
2024-01-31 17:23:40 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh" // string
+let age = 18 // number
+let isLoggedIn = false  // boolean
+let state;  // undefined 
+
+// number => 2 to power 53
+// bigint  -> used for very big numbers
+// string => ""   // we can use single quote also
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof "hitesh") // string
+console.log(typeof undefined); // undefined
+
+console.log(typeof null); // object  (Imp: interview question)
