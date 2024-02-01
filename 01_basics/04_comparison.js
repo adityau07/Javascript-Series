ae2c9aff138e7fc22d5a095e0bdf1a371a5aa13a
adityau07 <100702540+adityau07@users.noreply.github.com>
2024-02-01 12:30:29 +0000

    comparison of datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+// Video 8: Comparison of Datatypes in Javascript
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// Below all are confusing comparisons:
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===   (strict check)
+
+console.log("2" == 2);
+console.log("2" === 2);
