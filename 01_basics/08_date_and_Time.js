ea25916c8c74e3d2b9c69bfb90a54380e9d345bf
adityau07 <100702540+adityau07@users.noreply.github.com>
2024-02-02 15:11:30 +0000

    Updated Date and Time in JS

--- 01_basics/08_date_and_Time.js
+++ 01_basics/08_date_and_Time.js
@@ -1,4 +1,39 @@
-// Video 13: Date and Tieme in Javscript
+// Video 13: Date and Time in Javscript
+// go through it again: explore it by yourself
 
+// Date : Date is an object in javascript. 
 let myDate = new Date
 console.log(myDate)
+console.log(typeof myDate)
+console.log(myDate.toString())
+
+// We can use various methods to convert this date into more readable value.
+console.log("Another methods:")
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toJSON())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleTimeString())
+
+// *****************************************
+// let myCreatedDate = new Date("2023-01-14")
+//let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+//*********************************************
+
+let myDate1 = new Date("2023-01-14")
+console.log(myDate1)
+
+//** Doubt: below code 
+myDate1.toLocaleString('default',{
+    weekday: "long",
+})
+console.log(myDate1)
