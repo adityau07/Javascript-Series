dde7ee405c2e0e69d644df31f472f7492c56f9c7
adityau07 <100702540+adityau07@users.noreply.github.com>
2024-02-05 10:00:10 +0000

    objects in js

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,17 @@
+// Video 16: Objects in Javacsript
+
+// singleton
+// Object.create
+
+// object literals
+
+const JsUser = {
+    name: "Hitesh",
+    age: 18,
+    location:"Jaipur",
+    email:"hitesh@google.com",
+    isLoggedin: false,
+    lastLoginDays:["Monday","Saturday"]
+}
+
+console.log("Hello")
