cab60d5dc267f592b09de453fb8d3ea6b143ccbb
adityau07 <100702540+adityau07@users.noreply.github.com>
2024-02-11 15:08:32 +0000

    loop 2

---
+++ 05_iterations1/02_loop2.js
@@ -0,0 +1,24 @@
+// Video 28: while and do while loop
+
+//************************ while loop
+let index=0
+while(index<=10){
+    //console.log(`Value of index is: ${index}`)
+    index=index+2
+}
+
+//************************ while loop: array
+
+let myArr = ['John','Monty','Rohan']
+let arr=0
+while(arr<myArr.length){
+    //console.log(`Value is: ${myArr[arr]}`)
+    arr = arr+1
+}
+
+//****************** do while
+let i=0
+do {
+    console.log(i);
+    i++;
+} while (i<=10);
