812a86467726007e0c275e89c5ed4e86e968e1c1
adityau07 <100702540+adityau07@users.noreply.github.com>
2024-02-24 08:44:58 +0000

    DOM Apna College

---
+++ 07_DOM_Apna_College/script.js
@@ -0,0 +1,29 @@
+// Apna College DOM Video (from Javascript 12hrs)
+
+console.log("Hello")
+
+//let heading = document.getElementById("heading")
+//console.dir(heading)
+
+//let headings = document.getElementsByClassName("heading-class")
+//console.dir(headings)
+//console.log(headings)
+
+//let paras = document.getElementsByTagName("p")
+//console.dir(paras)
+
+//let firstElement = document.querySelector("p")
+//console.dir(firstElement)
+
+//let allElements = document.querySelectorAll("p")
+//console.dir(allElements)
+
+//let element = document.querySelector("#myID")
+//console.dir(element)
+//console.log(element.tagName)
+
+let element = document.querySelector("div")
+console.dir(element)
+//console.log(element.innerText)
+//console.log(element.innerHTML)
+
